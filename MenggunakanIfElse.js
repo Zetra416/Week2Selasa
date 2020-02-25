ebbcba7b407f99b634e365403687272e630af800
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-02-25 19:34:52 +0700

    menambah file screenshot code academy, dan 2 file js

---
+++ MenggunakanIfElse.js
@@ -0,0 +1,31 @@
+
+//var nama;
+var peran;
+
+var nama1 = "Thomas";
+var nama2 = "John";
+var nama3 = "Arthur";
+
+var peran1= "Ksatria";
+var peran2= "Tabib";
+var peran3= "Penyihir";
+
+var nama =window.prompt();
+console.log(nama);
+
+/*nama = nama1;
+peran = peran3;
+console.log("Nama harus diisi!");
+if (nama === nama1) {
+  console.log("Hallo" + nama + ", Pilih peranmu untuk memulai game!");
+  console.log("Selamat datang di Dunia Proxytia, " + nama);
+    if (peran === peran1) {
+      console.log("Halo " + peran + " " + nama + ", kamu dapat menyerang dengan senjatamu!");
+      }
+      else if (peran === peran2) {
+        console.log("Halo " + peran + " " + nama + ", kamu aka membantu temanmu yang terluka.");
+      }
+      else {
+        console.log("Halo " + peran + " " + nama + ", ciptakan keajaiban yang membantu kemenanganmu! ");
+      }
+}*/
